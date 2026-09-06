22a98a604c19821d25af94947de5e1b6dc666c9b
faltuwja-beep <faltuwja@gmail.com>
2026-09-06 17:12:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telebot.types import (
 )
 
 # Configuration & Credentials
-TOKEN = "8765709173:AAHNEVm4FcT86L7_RJZw5ihUd-uZtXf_I2E"
+TOKEN = "8765709173:AAGGxm08W3vCr2sN_5g8OZ34rFZqrYNSl6M"
 ADMIN_ID = 7161571409
 REQUIRED_CHANNEL = "@eraningwithask"
 
